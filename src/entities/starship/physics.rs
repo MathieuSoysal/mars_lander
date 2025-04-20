1832c74a97f236f566e8f60cb2ec877d96dbb8c9
hsoysal <hsoysal@student.42.fr>
2025-04-20 18:38:54 +0200

    Enhance starship movement logic with speed limits and power-based thrust

--- src/entities/starship/physics.rs
+++ src/entities/starship/physics.rs
@@ -4,15 +4,29 @@ const MARS_GRAVITY: f32 = 3.711;
 
 impl Starship {
     pub fn apply_movement(&mut self) {
-        let v0 = self.y_speed;
+        let v0_y = self.y_speed;
+        let v0_x = self.x_speed;
+        let rotation = self.rotation as f32;
+        let power = self.power as f32;
 
-        let v1 = -MARS_GRAVITY;
+        let v1_x = rotation.to_radians().sin() * power;
+        let v1_y = rotation.to_radians().cos() * power - MARS_GRAVITY;
 
-        self.y_speed += v1;
+        self.y_speed += v1_y;
+        if self.y_speed < -500. {
+            self.y_speed = -500.;
+        } else if self.y_speed > 500. {
+            self.y_speed = 500.;
+        }
+        self.x_speed += v1_x;
+        if self.x_speed < -500. {
+            self.x_speed = -500.;
+        } else if self.x_speed > 500. {
+            self.x_speed = 500.;
+        }
 
-        let calculated_y = v0 + (v1 / 2.);
-
-        self.y += calculated_y;
+        self.y += v0_y + (v1_y / 2.);
+        self.x += v0_x + (v1_x / 2.);
     }
 }
 
@@ -24,14 +38,20 @@ mod tests {
     #[test]
     fn test_apply_movement() {
         let mut starship = Starship::new(1000., 2700., 10000, 0, 0, 0., 0.);
-        for i in 0..20 {
+        for _ in 0..20 {
             starship.apply_movement();
-            println!(
-                "{i} x: {}, y: {}",
-                starship.get_x(),
-                starship.get_y()
-            );
         }
         assert_eq!(starship.get_y(), 1958);
+    }
+
+
+    #[test]
+    fn test_apply_movement_with_power1() {
+        let mut starship = Starship::new(1000., 2700., 10000, 0, 0, 0., 0.);
+        starship.add_power(1);
+        for _ in 0..20 {
+            starship.apply_movement();
+        }
+        assert_eq!(starship.get_y(), 2158);
     }
 }
