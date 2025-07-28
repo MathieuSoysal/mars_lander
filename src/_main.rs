34183280b9295fa450b0864673c9b5454efb9da5
solvictor <victor.egret.pro@gmail.com>
2025-07-28 23:47:14 +0200

    better weights

--- src/_main.rs
+++ src/_main.rs
@@ -1,186 +1,72 @@
+use std::{io, ops::Div};
+
 use itertools::Itertools;
-use rand::distributions::Bernoulli;
-use rand::prelude::*;
-use std::io;
 
-macro_rules! parse_input {
-    ($x:expr, $t:ident) => {
-        $x.trim().parse::<$t>().unwrap()
-    };
+#[derive(Debug, Clone, Copy)]
+struct SimulationParams {
+    pub pop_size: usize,
+    pub nb_generations: i32,
+    pub crossover_rate: f64,
+    pub mutation_rate: f64,
+    pub elite_rate: f64,
 }
 
-use std::ops::Div as _;
-
-const GEN_POWER_SIZE_BITS: usize = 2;
-const GEN_ROTATE_SIZE_BITS: usize = 2;
-const GEN_MASK_POWER: u8 = (1 << GEN_POWER_SIZE_BITS) - 1;
-const GEN_MASK_ROTATE: u8 = (1 << GEN_ROTATE_SIZE_BITS) - 1;
-
-pub const GENOME_SIZE: usize = 100;
-
-pub const MARS_GRAVITY: f32 = 3.711;
-const MAX_H_SPEED_ON_LAND: f32 = 20.;
-const MAX_V_SPEED_ON_LAND: f32 = 40.;
-const ANGLE_TO_LAND: i32 = 0;
-const MAX_X: i32 = 6999;
-const MAX_Y: i32 = 2999;
-pub const WIDTH: usize = 7000;
-pub const HEIGHT: usize = 3000;
-
-#[derive(Clone)]
-pub struct Point {
-    pub x: usize,
-    pub y: usize,
+#[derive(Clone, Copy)]
+pub struct Starship {
+    pub x: i32,
+    pub y: i32,
+    pub fuel: u16,
+    pub rotation: i8,
+    pub power: u8,
+    pub x_speed: f32,
+    pub y_speed: f32,
 }
 
-#[derive(Clone)]
-pub struct Segment {
-    pub start: Point,
-    pub end: Point,
-}
+const MIN_ROTATE: i8 = -90;
+const MAX_ROTATE: i8 = 90;
+const MAX_POWER: u8 = 4;
 
-impl Segment {
-    pub fn is_landing(&self) -> bool {
-        self.start.y == self.end.y
-    }
-}
-
-pub struct Game {
-    points: Vec<Point>,
-    segments: Vec<Segment>,
-    crash_points: [u32; WIDTH], // Dans le puzzle lv 3 cela ne marchera plus
-    landing: Segment,
-}
-
-impl Game {
-    pub fn new(nb_points: usize) -> Self {
-        let points = Vec::with_capacity(nb_points);
-        let segments = Vec::with_capacity(nb_points);
-        Game {
-            points,
-            segments,
-            crash_points: [0; WIDTH],
-            landing: Segment {
-                start: Point { x: 0, y: 0 },
-                end: Point { x: 0, y: 0 },
-            },
+impl Starship {
+    pub fn copy(&self) -> Self {
+        Starship {
+            x: self.x,
+            y: self.y,
+            fuel: self.fuel,
+            rotation: self.rotation,
+            power: self.power,
+            x_speed: self.x_speed,
+            y_speed: self.y_speed,
         }
     }
 
-    pub fn get_distance_to_landing(&self, starship: &Starship) -> (i32, i32) {
-        let x = starship.get_x();
-        let y = starship.get_y();
-
-        let dist_x = if x >= self.landing.start.x as i32 && x <= self.landing.end.x as i32 {
-            0
-        } else {
-            let d_start_x = (self.landing.start.x as i32 - x).abs();
-            let d_end_x = (self.landing.end.x as i32 - x).abs();
-            d_end_x.min(d_start_x) as i32
-        };
-
-        let dist_y = if y >= self.landing.start.y as i32 && y <= self.landing.end.y as i32 {
-            0
-        } else {
-            let d_start_y = (self.landing.start.y as i32 - y).abs();
-            let d_end_y = (self.landing.end.y as i32 - y).abs();
-            d_end_y.min(d_start_y) as i32
-        };
-        (dist_x, dist_y)
-    }
-
-    pub fn starship_is_crash(&self, starship: &Starship) -> bool {
-        let x = starship.get_x();
-        let y = starship.get_y();
-        let is_on_landing = x as usize >= self.landing.start.x
-            && x as usize <= self.landing.end.x
-            && y as usize <= self.landing.start.y;
-        x < 0
-            || x > MAX_X
-            || y > MAX_Y
-            || y < 0
-            || self.crash_points[x as usize] >= y as u32
-            || is_on_landing
-    }
-
-    pub fn starship_is_landing(&self, starship: &Starship) -> bool {
-        let x = starship.get_x() as usize;
-        let y = starship.get_y() as usize;
-        x >= self.landing.start.x
-            && x <= self.landing.end.x
-            && y <= self.landing.start.y
-            && starship.get_x_speed().abs() <= MAX_H_SPEED_ON_LAND
-            && starship.get_y_speed().abs() <= MAX_V_SPEED_ON_LAND
-            && starship.get_rotation() == ANGLE_TO_LAND as i8
-    }
-
-    pub fn add_point(&mut self, x: usize, y: usize) {
-        if !self.points.is_empty() {
-            let start = self.points.last().unwrap().clone();
-            let end = Point { x, y };
-            self.add_segment(Segment { start, end });
-        }
-        self.points.push(Point { x, y });
-    }
-
-    fn add_segment(&mut self, seg: Segment) {
-        if seg.is_landing() {
-            self.landing = seg.clone();
-        } else {
-            let start_x = seg.start.x as i32;
-            let start_y = seg.start.y as i32;
-            let end_y = seg.end.y as i32;
-            let end_x = seg.end.x as i32;
-            self.segments.push(seg);
-            for x in start_x..=end_x {
-                self.crash_points[x as usize] = ((((end_y - start_y) * x) / (end_x - start_x))
-                    + (start_y + -1 * ((start_x * (end_y - start_y)) / (end_x - start_x))))
-                    as u32;
-            }
+    pub fn new(
+        x: i32,
+        y: i32,
+        fuel: u16,
+        rotation: i8,
+        power: u8,
+        x_speed: f32,
+        y_speed: f32,
+    ) -> Self {
+        Starship {
+            x: (x * 100),
+            y: (y * 100),
+            fuel,
+            rotation,
+            power,
+            x_speed,
+            y_speed,
         }
     }
-}
 
-type Nucleotide = u8;
-pub type Genome = [Nucleotide; GENOME_SIZE];
-
-pub fn gen_init_rand() -> Genome {
-    let mut genome = [0; GENOME_SIZE];
-    for i in 0..GENOME_SIZE {
-        genome[i] = ((random::<u8>() % 3) << GEN_ROTATE_SIZE_BITS) | (random::<u8>() % 3);
+    #[inline(always)]
+    pub fn get_x(&self) -> i32 {
+        (self.x + 50) / 100
     }
-    genome
-}
 
-pub fn gen_init_full() -> Genome {
-    let genome = [2 << 2 | 2; GENOME_SIZE];
-    genome
-}
-
-pub fn gen_init_semi_full() -> Genome {
-    let genome = [2 << 2 | 1; GENOME_SIZE];
-    genome
-}
-
-pub fn get_rotate_on_turn(genome: &Genome, nb_turn: usize) -> i8 {
-    let turn = genome[nb_turn];
-    let rotate = turn & GEN_MASK_ROTATE as u8;
-    match rotate {
-        0 => 0,
-        1 => -15,
-        2 => 15,
-        _ => 0,
-    }
-}
-
-pub fn get_power_on_turn(genome: &Genome, nb_turn: usize) -> i8 {
-    let turn = genome[nb_turn];
-    let power = (turn >> GEN_ROTATE_SIZE_BITS) & GEN_MASK_POWER as u8;
-    match power {
-        0 => 0,
-        1 => -1,
-        2 => 1,
-        _ => 0,
+    #[inline(always)]
+    pub fn get_y(&self) -> i32 {
+        (self.y + 50) / 100
     }
 }
 
@@ -244,6 +130,186 @@ impl Starship {
     }
 }
 
+const MARS_GRAVITY: f32 = 3.711;
+const MAX_H_SPEED_ON_LAND: f32 = 20.;
+const MAX_V_SPEED_ON_LAND: f32 = 40.;
+const ANGLE_TO_LAND: i32 = 0;
+const MAX_X: i32 = 6999;
+const MAX_Y: i32 = 2999;
+
+#[derive(Clone)]
+struct Point {
+    pub x: usize,
+    pub y: usize,
+}
+
+#[derive(Clone)]
+struct Segment {
+    pub start: Point,
+    pub end: Point,
+}
+
+impl Segment {
+    pub fn is_landing(&self) -> bool {
+        self.start.y == self.end.y
+    }
+}
+
+struct Game {
+    points: Vec<Point>,
+    segments: Vec<Segment>,
+    landing: Segment,
+}
+
+#[inline(always)]
+fn orientation(p: &Point, q: &Point, r: &Point) -> i32 {
+    let val = (q.y as i32 - p.y as i32) * (r.x as i32 - q.x as i32)
+        - (q.x as i32 - p.x as i32) * (r.y as i32 - q.y as i32);
+    if val == 0 {
+        0 // colinear
+    } else if val > 0 {
+        1 // clockwise
+    } else {
+        2 // counter-clockwise
+    }
+}
+
+#[inline(always)]
+fn on_segment(p: &Point, q: &Point, r: &Point) -> bool {
+    q.x >= p.x.min(r.x) && q.x <= p.x.max(r.x) && q.y >= p.y.min(r.y) && q.y <= p.y.max(r.y)
+}
+
+#[inline(always)]
+fn collide(seg1: &Segment, seg2: &Segment) -> bool {
+    let p1 = &seg1.start;
+    let q1 = &seg1.end;
+    let p2 = &seg2.start;
+    let q2 = &seg2.end;
+
+    let o1 = orientation(p1, q1, p2);
+    let o2 = orientation(p1, q1, q2);
+    let o3 = orientation(p2, q2, p1);
+    let o4 = orientation(p2, q2, q1);
+
+    // General case
+    if o1 != o2 && o3 != o4 {
+        return true;
+    }
+
+    // Special cases
+    if o1 == 0 && on_segment(p1, p2, q1) {
+        return true;
+    }
+    if o2 == 0 && on_segment(p1, q2, q1) {
+        return true;
+    }
+    if o3 == 0 && on_segment(p2, p1, q2) {
+        return true;
+    }
+    if o4 == 0 && on_segment(p2, q1, q2) {
+        return true;
+    }
+
+    false
+}
+
+impl Game {
+    pub fn new(nb_points: usize) -> Self {
+        let points = Vec::with_capacity(nb_points);
+        let segments = Vec::with_capacity(nb_points);
+        Game {
+            points,
+            segments,
+            landing: Segment {
+                start: Point { x: 0, y: 0 },
+                end: Point { x: 0, y: 0 },
+            },
+        }
+    }
+
+    pub fn get_distance_to_landing(&self, starship: &Starship) -> (i32, i32) {
+        let x = starship.get_x();
+        let y = starship.get_y();
+
+        let dist_x = if x >= self.landing.start.x as i32 && x <= self.landing.end.x as i32 {
+            0
+        } else {
+            let d_start_x = (self.landing.start.x as i32 - x).abs();
+            let d_end_x = (self.landing.end.x as i32 - x).abs();
+            d_end_x.min(d_start_x) as i32
+        };
+
+        let dist_y = if y >= self.landing.start.y as i32 && y <= self.landing.end.y as i32 {
+            0
+        } else {
+            let d_start_y = (self.landing.start.y as i32 - y).abs();
+            let d_end_y = (self.landing.end.y as i32 - y).abs();
+            d_end_y.min(d_start_y) as i32
+        };
+        (dist_x, dist_y)
+    }
+
+    fn collide_seg(&self, starship: &Starship, px: i32, py: i32) -> bool {
+        let cx = starship.get_x();
+        let cy = starship.get_y();
+
+        let segment = Segment {
+            start: Point {
+                x: px as usize,
+                y: py as usize,
+            },
+            end: Point {
+                x: cx as usize,
+                y: cy as usize,
+            },
+        };
+
+        self.segments.iter().any(|seg| collide(seg, &segment)) // TODO reduce number of segments 
+    }
+
+    pub fn starship_is_crash(&self, starship: &Starship, px: i32, py: i32) -> bool {
+        let x = starship.get_x();
+        let y = starship.get_y();
+        let is_on_landing = x as usize >= self.landing.start.x
+            && x as usize <= self.landing.end.x
+            && y as usize <= self.landing.start.y;
+        x < 0
+            || x > MAX_X
+            || y > MAX_Y
+            || y < 0
+            || is_on_landing
+            || self.collide_seg(starship, px, py)
+    }
+
+    pub fn starship_is_landing(&self, starship: &Starship) -> bool {
+        let x = starship.get_x() as usize;
+        let y = starship.get_y() as usize;
+        x >= self.landing.start.x + 50
+            && x <= self.landing.end.x - 50
+            && y <= self.landing.start.y
+            && starship.x_speed.abs() <= MAX_H_SPEED_ON_LAND
+            && starship.y_speed.abs() <= MAX_V_SPEED_ON_LAND
+            && starship.rotation == ANGLE_TO_LAND as i8
+    }
+
+    pub fn add_point(&mut self, x: usize, y: usize) {
+        if !self.points.is_empty() {
+            let start = self.points.last().unwrap().clone();
+            let end = Point { x, y };
+            self.add_segment(Segment { start, end });
+        }
+        self.points.push(Point { x, y });
+    }
+
+    fn add_segment(&mut self, seg: Segment) {
+        if seg.is_landing() {
+            self.landing = seg.clone();
+        } else {
+            self.segments.push(seg);
+        }
+    }
+}
+
 impl Starship {
     #[inline(always)]
     pub fn apply_movement(&mut self) {
@@ -270,37 +336,53 @@ impl Starship {
     }
 }
 
+use rand::{distributions::Bernoulli, prelude::*};
+
+const GEN_POWER_SIZE_BITS: usize = 2;
+const GEN_ROTATE_SIZE_BITS: usize = 2;
+const GEN_MASK_POWER: u8 = (1 << GEN_POWER_SIZE_BITS) - 1;
+const GEN_MASK_ROTATE: u8 = (1 << GEN_ROTATE_SIZE_BITS) - 1;
+
+const GENOME_SIZE: usize = 100;
+
+type Nucleotide = u8;
+type Genome = [Nucleotide; GENOME_SIZE];
+
+fn gen_init_rand() -> Genome {
+    let mut genome = [0; GENOME_SIZE];
+    for i in 0..GENOME_SIZE {
+        genome[i] = ((random::<u8>() % 3) << GEN_ROTATE_SIZE_BITS) | (random::<u8>() % 3);
+    }
+    genome
+}
+
+fn get_rotate_on_turn(genome: &Genome, nb_turn: usize) -> i8 {
+    let turn = genome[nb_turn];
+    let rotate = turn & GEN_MASK_ROTATE as u8;
+    match rotate {
+        0 => 0,
+        1 => -15,
+        2 => 15,
+        _ => 0,
+    }
+}
+
+fn get_power_on_turn(genome: &Genome, nb_turn: usize) -> i8 {
+    let turn = genome[nb_turn];
+    let power = (turn >> GEN_ROTATE_SIZE_BITS) & GEN_MASK_POWER as u8;
+    match power {
+        0 => 0,
+        1 => -1,
+        2 => 1,
+        _ => 0,
+    }
+}
+
 #[derive(Clone, Copy)]
-pub struct DNA {
+struct DNA {
     genome: Genome,
     starship: Starship,
     fitness: f64,
-}
-
-impl DNA {
-    pub fn new(genome: Genome, starship: Starship) -> Self {
-        DNA {
-            genome,
-            starship,
-            fitness: -1.,
-        }
-    }
-
-    pub fn fuel_left(&self, game: &Game) -> u16 {
-        let mut s = self.starship.copy();
-        for i in 0..GENOME_SIZE {
-            let rotate = get_rotate_on_turn(&self.genome, i);
-            let power = get_power_on_turn(&self.genome, i);
-            s.add_power(power as i16);
-            s.add_rotation(rotate);
-            s.apply_movement();
-
-            if game.starship_is_landing(&s) || game.starship_is_crash(&s) {
-                break;
-            }
-        }
-        s.get_fuel()
-    }
 }
 
 const LAND_DISTANCE_X_WEIGHT: f64 = 20.0;
@@ -310,8 +392,11 @@ const X_SPEED_WEIGHT: f64 = 4.0;
 const Y_SPEED_WEIGHT: f64 = 5.0;
 const FUEL_WEIGHT: f64 = 100.0;
 
-pub const WINNING_FITNESS: f64 =
-    LAND_DISTANCE_X_WEIGHT + ROTATION_WEIGHT + X_SPEED_WEIGHT + Y_SPEED_WEIGHT;
+pub const WINNING_FITNESS: f64 = LAND_DISTANCE_X_WEIGHT
+    + LAND_DISTANCE_Y_WEIGHT
+    + ROTATION_WEIGHT
+    + X_SPEED_WEIGHT
+    + Y_SPEED_WEIGHT;
 
 #[inline(always)]
 fn calc_fit(
@@ -331,6 +416,14 @@ fn calc_fit(
 }
 
 impl DNA {
+    pub fn new(genome: Genome, starship: Starship) -> Self {
+        DNA {
+            genome,
+            starship,
+            fitness: -1.,
+        }
+    }
+
     pub fn fitness(&mut self, game: &Game) -> f64 {
         if self.fitness != -1.0 {
             return self.fitness;
@@ -338,6 +431,8 @@ impl DNA {
         self.fitness = 0.0;
         let mut s = self.starship.copy();
         for i in 0..GENOME_SIZE {
+            let px = s.get_x();
+            let py = s.get_y();
             let rotate = get_rotate_on_turn(&self.genome, i);
             let power = get_power_on_turn(&self.genome, i);
             s.add_power(power as i16);
@@ -345,18 +440,18 @@ impl DNA {
             s.apply_movement();
 
             if game.starship_is_landing(&s) {
-                self.fitness = calc_fit(0, 0, 0, 0., 0., s.get_fuel());
+                self.fitness = calc_fit(0, 0, 0, 0., 0., s.fuel);
                 break;
             }
 
-            if game.starship_is_crash(&s) {
+            if game.starship_is_crash(&s, px, py) {
                 let (land_dist_x, land_dist_y) = game.get_distance_to_landing(&s);
                 self.fitness = calc_fit(
                     land_dist_x,
                     land_dist_y,
-                    s.get_rotation(),
-                    s.get_x_speed(),
-                    s.get_y_speed(),
+                    s.rotation,
+                    s.x_speed,
+                    s.y_speed,
                     0,
                 );
                 break;
@@ -389,112 +484,12 @@ impl DNA {
     pub fn get_genome(&self) -> Genome {
         self.genome
     }
-
-    pub fn next_move(&self) -> (i8, i8) {
-        let rotate = get_rotate_on_turn(&self.genome, 0);
-        let power = get_power_on_turn(&self.genome, 0);
-        (rotate, power)
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct SimulationParams {
-    pub pop_size: usize,
-    pub nb_generations: i32,
-    pub crossover_rate: f64,
-    pub mutation_rate: f64,
-    pub elite_rate: f64,
-}
-
-#[derive(Clone, Copy)]
-pub struct Starship {
-    pub x: i32,
-    pub y: i32,
-    pub fuel: u16,
-    pub rotation: i8,
-    pub power: u8,
-    pub x_speed: f32,
-    pub y_speed: f32,
-}
-
-const MIN_ROTATE: i8 = -90;
-const MAX_ROTATE: i8 = 90;
-
-const MAX_POWER: u8 = 4;
-
-impl Starship {
-    pub fn copy(&self) -> Self {
-        Starship {
-            x: self.x,
-            y: self.y,
-            fuel: self.fuel,
-            rotation: self.rotation,
-            power: self.power,
-            x_speed: self.x_speed,
-            y_speed: self.y_speed,
-        }
-    }
-
-    pub fn new(
-        x: i32,
-        y: i32,
-        fuel: u16,
-        rotation: i8,
-        power: u8,
-        x_speed: f32,
-        y_speed: f32,
-    ) -> Self {
-        Starship {
-            x: (x * 100),
-            y: (y * 100),
-            fuel,
-            rotation,
-            power,
-            x_speed,
-            y_speed,
-        }
-    }
-
-    #[inline(always)]
-    pub fn get_x(&self) -> i32 {
-        (self.x + 50) / 100
-    }
-
-    #[inline(always)]
-    pub fn get_y(&self) -> i32 {
-        (self.y + 50) / 100
-    }
-
-    #[inline(always)]
-    pub fn get_fuel(&self) -> u16 {
-        self.fuel
-    }
-
-    #[inline(always)]
-    pub fn get_rotation(&self) -> i8 {
-        self.rotation
-    }
-
-    #[inline(always)]
-    pub fn get_power(&self) -> u8 {
-        self.power
-    }
-
-    #[inline(always)]
-    pub fn get_x_speed(&self) -> f32 {
-        self.x_speed
-    }
-
-    #[inline(always)]
-    pub fn get_y_speed(&self) -> f32 {
-        self.y_speed
-    }
 }
 
 const TOUR_SIZE: usize = 5;
 
 // Returns the best individual
-pub fn elitiste_new_population(
+fn elitiste_new_population(
     population: &mut [DNA],
     new_population: &mut [DNA],
     elite_count: usize,
@@ -548,7 +543,7 @@ pub fn elitiste_new_population(
     best
 }
 
-pub fn run_simulation(game: &Game, starship: &Starship, params: &SimulationParams) -> Option<DNA> {
+fn run_simulation(game: &Game, starship: &Starship, params: &SimulationParams) -> Option<DNA> {
     let mut population = (0..params.pop_size)
         .map(|_| {
             let genome = gen_init_rand();
@@ -589,6 +584,12 @@ pub fn run_simulation(game: &Game, starship: &Starship, params: &SimulationParam
     overall_best
 }
 
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => {
+        $x.trim().parse::<$t>().unwrap()
+    };
+}
+
 fn main() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
@@ -610,8 +611,8 @@ fn main() {
     }
 
     let params = SimulationParams {
-        pop_size: 500,
-        nb_generations: 500,
+        pop_size: 200,
+        nb_generations: 200,
         crossover_rate: 0.96,
         mutation_rate: 0.055,
         elite_rate: 0.06,
@@ -643,8 +644,7 @@ fn main() {
         starship.add_rotation(rotate);
         starship.apply_movement();
 
-        // R P. R is the desired rotation angle. P is the desired thrust power.
-        println!("{} {}", starship.get_rotation(), starship.get_power());
+        println!("{} {}", starship.rotation, starship.power);
         i += 1;
     }
 }
