be2b117dab85fdd7a992af80c04247c31f15e692
Mathieu Soysal <hsoysal@student.42.fr>
2026-01-14 20:56:55 +0100

    Implement efficient caching integration with Cloudflare Pages and remove unnecessary files

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    root: 'web',
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+        assetsInlineLimit: 0,
+        rollupOptions: {
+            output: {
+                entryFileNames: '[name].[hash].js',
+                chunkFileNames: '[name].[hash]. js',
+                assetFileNames: '[name].[hash].[ext]',
+            },
+        },
+    },
+});
